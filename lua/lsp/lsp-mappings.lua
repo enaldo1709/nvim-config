e96836a0271586b21731904e682c76a299eda8e1
Enaldo Narváez Yepes <enaldo_17@hotmail.com>
2023-09-13 14:47:22 -0500

    feat: add completion mappings

---
+++ lua/lsp/lsp-mappings.lua
@@ -0,0 +1,6 @@
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gr", vim.lsp.buf.references)
+vim.keymap.set("n", "gh", vim.lsp.buf.hover)
+vim.keymap.set("n", "gnr", vim.lsp.buf.rename)
+vim.keymap.set("n", "ge", ":TroubleToggle <CR>")
